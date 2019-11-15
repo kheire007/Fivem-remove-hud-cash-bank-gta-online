c59902af3fa58ad50f6119e9b935cccb61f1280b
kheire007 <36076014+kheire007@users.noreply.github.com>
2019-11-15 02:54:26 +0100

    Add files via upload

---
+++ removecashbank/client.lua
@@ -0,0 +1,10 @@
+-------Made by Sarah----------
+
+-- REMOVE_MULTIPLAYER_HUD_CASH/HUD_BANK
+
+Citizen.CreateThread(function()
+    RemoveMultiplayerHudCash(0x968F270E39141ECA)
+    RemoveMultiplayerBankCash(0xC7C6789AA1CFEDD0)
+end)
+
+
