5b5559ede75c0120f4a1de195ccde6272737fabb
kheire007 <36076014+kheire007@users.noreply.github.com>
2019-12-30 17:29:47 +0100

    Update client.lua

--- removecashbank/client.lua
+++ removecashbank/client.lua
@@ -1,7 +1,5 @@
 -------Made by Sarah----------
 
--- REMOVE_MULTIPLAYER_HUD_CASH/HUD_BANK
-
 Citizen.CreateThread(function()
     RemoveMultiplayerHudCash(0x968F270E39141ECA)
     RemoveMultiplayerBankCash(0xC7C6789AA1CFEDD0)
